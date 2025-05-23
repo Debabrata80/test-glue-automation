8cd3b036a08fe5a96783d65ed32f05ad0fbbf6e1
Debabrata <debab1234@gmail.com>
2025-05-23 17:24:20 +0530

    glue job automation

--- script-path/job-script.py
+++ script-path/job-script.py
@@ -6,7 +6,7 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 
 # Arguments passed to the job
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'SOURCE_S3_PATH', 'TARGET_S3_PATH'])
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'SOURCE_PATH', 'TARGET_PATH'])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -16,12 +16,12 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 # Read CSV from source path in S3
-df = spark.read.option("header", True).csv(args['SOURCE_S3_PATH'])
+df = spark.read.option("header", True).csv(args['SOURCE_PATH'])
 
 # OPTIONAL: Transform or clean data
 # Example: df = df.dropDuplicates()
 
 # Write CSV to target path
-df.write.mode("overwrite").option("header", True).csv(args['TARGET_S3_PATH'])
+df.write.mode("overwrite").option("header", True).parquet(args['TARGET_PATH'])
 
 job.commit()
